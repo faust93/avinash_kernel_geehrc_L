ef6d550bc8ee338403675c0f661f1226c36d590c
SaiAvinash92 <aviz1911@gmail.com>
2014-11-17 18:33:37 +0530

    Several build script changes to make things easy :D

--- build.sh
+++ build.sh
@@ -1,7 +1,16 @@
+#!/bin/bash
+
+make clean
+make mrproper
+export ARCH=arm
+export SUBARCH=arm
+version="1"
+make avinash_defconfig
+
 if [ $# -gt 0 ]; then
 echo $1 > .version
 fi
- 
+
 make -j7
  
 cp arch/arm/boot/zImage ../ramdisk_mako/
@@ -18,7 +27,7 @@ rm -rf zImage
  
 cd ../mako/
  
-zipfile="nash-Nightly.zip"
+zipfile="nash.Kernel-r$version.zip"
 echo "making zip file"
 cp boot.img zip/
  
@@ -29,3 +38,7 @@ rm -f *.zip
 zip -r $zipfile *
 rm -f /tmp/*.zip
 cp *.zip /tmp
+mv *.zip /home/avinash/Android/FlashZip
+
+rm -rf boot.img
+rm -rf ../boot.img
